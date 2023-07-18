7a9b7a7692639be53a24854f57401e7e0c5d71e9
0-mayurkaretha <0.mayurkaretha@gmail.com>
2023-07-18 09:03:32 +0530

    complete

---
+++ server/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const UserModel = require('./models/Users')
+
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect("mongodb+srv://Mark:MARKkarter@cluster0.ndtukx4.mongodb.net/MernPractice?retryWrites=true&w=majority");
+
+//get data from mongodb server
+app.get("/getUsers", (req, res) => {
+    UserModel.find({}).then((err, result) => {
+        if(err){
+            res.json(err);
+        }else{
+            res.json(result)
+        }
+    });
+});
+
+
+//post data in mongodb server after getting from frontend interactions
+app.post("/createUser", async (req, res) => {
+    const user = req.body;
+    const newUser = new UserModel(user);
+    await newUser.save();
+
+    res.json(user);
+});
+
+
+//
+app.listen(3001, () => {
+    console.log("server runs good.")
+});
